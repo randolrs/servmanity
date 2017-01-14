954f2f0c6db54c6df6f502141facc0ae0405fe77
randolrs <r.shane.randolph@gmail.com>
2017-01-14 14:40:24 -0800

    causual racism of the sweaty underclass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def login
+
+  	@hide_header = true
+
+  end
+
+
+  def signup
+
+  	@hide_header = true
+  	
+  end
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #default URL
+
+  config.action_mailer.default_url_options = { host: 'https://servmanity.herokuapp.com/' }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
