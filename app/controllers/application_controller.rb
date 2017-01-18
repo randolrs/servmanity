f1c0dab817da16c16641d23dcae36de54e176498
randolrs <r.shane.randolph@gmail.com>
2017-01-18 14:51:48 -0800

    30 years ago

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,32 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+	protect_from_forgery with: :exception
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+	def after_sign_in_path_for(user)
+
+		root_path
+
+	end
+
+
+	def after_sign_out_path_for(user)
+
+		root_path
+
+	end
+
+
+	def configure_permitted_parameters
+
+		registration_params = [:email, :password, :first_name, :last_name, :zip_code, :phone_number, :is_tasker]
+		devise_parameter_sanitizer.permit(:sign_up, keys: registration_params)
+		devise_parameter_sanitizer.permit(:sign_in, keys: registration_params)
+		devise_parameter_sanitizer.permit(:account_update, keys: registration_params)
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'users/non_tasker_signup'
+  devise_for :users, controllers: {registrations: 'registrations'}
 
-  devise_for :users
+  get 'users/non_tasker_signup'
   
   root "pages#home"
 
