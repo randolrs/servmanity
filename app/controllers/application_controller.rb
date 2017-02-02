ef6f69ca676da653987e2fe42bdbcf0a0e570815
randolrs <r.shane.randolph@gmail.com>
2017-02-02 12:25:56 -0800

    future

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,17 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(user)
 
-		root_path
+		unless current_user.id
+			
+			root_path
+
+		else
+
+			root_path
+			
+		end
+
+
 
 	end
 
