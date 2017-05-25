193f4a02df148a48d24cf41d54ca709bcf707f0b
randolrs <r.shane.randolph@gmail.com>
2017-05-25 18:28:15 -0400

    stiff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
 	if Rails.env == "production"
 
-		Stripe.api_key = ENV['STRIPE_LIVE_SECRET_KEY']
+		Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY'] #NEED TO SWITCH TO LIVE ON LAUNCH
 
 	else
 
