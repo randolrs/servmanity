f0ef69cafbb674bd681c9661c287de0c2cab367e
randolrs <r.shane.randolph@gmail.com>
2017-05-26 08:35:38 -0400

    haters

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -3,14 +3,5 @@ class StripeController < ApplicationController
   
 #protect_from_forgery :except => :webhook
 
-  if Rails.env == "production"
-
-    Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
-
-  else
-
-    Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
-
-  end
 
 end
